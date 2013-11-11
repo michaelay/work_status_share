70129b34738048105af5c00e77f418cfe4282c26
Michael Au-Yeung <michael.auyeung@tvb.com>
2013-11-11 18:46:20 +0800

    put all js in one file

--- workstatus.js
+++ workstatus.js
@@ -1,5 +1,3 @@
-// Users = new Meteor.Collection("users");
-
 Meteor.users.allow({
   update: function(userId, docs, fields, modifier) { 
     return doc.owner === userId;
@@ -12,13 +10,23 @@ Meteor.users.allow({
   }
 });
 
+Accounts.loginServiceConfiguration.allow({
+  insert: function(userId, doc) {
+    return true;
+  },
+  remove: function(userId, doc) {
+    return true;
+  }
+});
+
+
 if (Meteor.isClient) {
 
   Template.status_list.users = function() { 
     return Meteor.users.find(
       {},
       {sort: {"profile.ws.last_update_time": -1}}
-    );
+      );
   };
 
   Template.status_list.users_count = function() { 
@@ -56,7 +64,7 @@ if (Meteor.isClient) {
           "profile.ws.status":text,
           "profile.ws.last_update_time":(new Date().getTime() / 1000.0)
         }}
-      );
+        );
     } 
   });
 
@@ -80,37 +88,45 @@ if (Meteor.isServer) {
       return Meteor.users.find(
         {},
         {fields: {profile:1, services:1}}
-      );
+        );
     });
 
-// Accounts.loginServiceConfiguration.remove({
-//   service: "facebook"
-// });
+    // account related
+    (function() { 
 
-Accounts.loginServiceConfiguration.insert({
-  service: "facebook",
-  // appId: 1376259485952394, 
-  // secret: "c297d3c967f73e4cd40a265de5ee05a9"
-  appId: 558946830850843,  // tvbcomstatus
-  secret: "4dda8c684f34e4931a423445ffc0b07d" // tvbcomstatus
-});
+      var allowed_accounts = ['michaelay@gmail.com'];
 
-// Accounts.validateNewUser(function(user) { 
-//   if (user.services.facebook && user.services.facebook.email) {
-//     var email = user.services.facebook.email;
-//     if (allowed_accounts.indexOf(email) === -1) {
-//       return true;
-//     } else { 
-//       return true;
-//     }
-//   }
-// });
 
-// Accounts.onCreateUser(function(options, user) {
-//   console.log("on create user");
-//   user.profile_picture_url = "https://graph.facebook.com/" + user.services.facebook.username + "/picture";
-//   return user;
-// });
-    
+      Accounts.loginServiceConfiguration.remove({
+        service: "facebook"
+      });
+
+      Accounts.loginServiceConfiguration.insert({
+        service: "facebook",
+        // appId: 1376259485952394, 
+        // secret: "c297d3c967f73e4cd40a265de5ee05a9"
+        appId: 558946830850843,  // tvbcomstatus
+        secret: "4dda8c684f34e4931a423445ffc0b07d" // tvbcomstatus
+      });
+
+      Accounts.validateNewUser(function(user) { 
+        if (user.services.facebook && user.services.facebook.email) {
+          var email = user.services.facebook.email;
+          if (allowed_accounts.indexOf(email) === -1) {
+            return true;
+          } else { 
+            return true;
+          }
+        }
+      });
+
+    // Accounts.onCreateUser(function(options, user) {
+    //   console.log("on create user");
+    //   user.profile_picture_url = "https://graph.facebook.com/" + user.services.facebook.username + "/picture";
+    //   return user;
+    // });
+
+    })();
+
   });
 }
