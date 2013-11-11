a149edb903ba10753a7e3eb0c7f1b202afb2b012
Michael Au-Yeung <michael.auyeung@tvb.com>
2013-11-11 17:28:42 +0800

    work on tvbcomstatus.meteor.com

--- accounts.js
+++ accounts.js
@@ -1,27 +1,36 @@
 allowed_accounts = ['michaelay@gmail.com'];
 
-Accounts.loginServiceConfiguration.remove({
-  service: "facebook"
-});
-
-Accounts.loginServiceConfiguration.insert({
-  service: "facebook",
-  appId: 1376259485952394, 
-  secret: "c297d3c967f73e4cd40a265de5ee05a9"
-  // appId: 558946830850843,  // tvbcomstatus
-  // secret: "4dda8c684f34e4931a423445ffc0b07d" // tvbcomstatus
-});
-
-Accounts.validateNewUser(function(user) { 
-  if (user.services.facebook && user.services.facebook.email) {
-    var email = user.services.facebook.email;
-    if (allowed_accounts.indexOf(email) === -1) {
-      return true;
-    } else { 
-      return true;
-    }
+Accounts.loginServiceConfiguration.allow({
+  insert: function(userId, doc) {
+    return true;
+  },
+  remove: function(userId, doc) {
+    return true;
   }
 });
+
+// Accounts.loginServiceConfiguration.remove({
+//   service: "facebook"
+// });
+
+// Accounts.loginServiceConfiguration.insert({
+//   service: "facebook",
+//   appId: 1376259485952394, 
+//   secret: "c297d3c967f73e4cd40a265de5ee05a9"
+//   // appId: 558946830850843,  // tvbcomstatus
+//   // secret: "4dda8c684f34e4931a423445ffc0b07d" // tvbcomstatus
+// });
+
+// Accounts.validateNewUser(function(user) { 
+//   if (user.services.facebook && user.services.facebook.email) {
+//     var email = user.services.facebook.email;
+//     if (allowed_accounts.indexOf(email) === -1) {
+//       return true;
+//     } else { 
+//       return true;
+//     }
+//   }
+// });
 
 // Accounts.onCreateUser(function(options, user) {
 //   console.log("on create user");
